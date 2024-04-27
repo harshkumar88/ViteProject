93a502284fd116242d582d6e6b581be38765deb2
harsh kumar <harsh.kumar@crofarm.in>
2024-04-27 15:41:49 +0530

    home

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const Button = ({ styles }) => (
-  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
-    Get Started
+const Button = ({ styles, text }) => (
+  <button
+    type="button"
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+  >
+    {text && text != undefined ? text : "Get Started"}
   </button>
 );
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,57 +1,80 @@
 import styles from "../style";
-import { robot } from "../assets";
 import GetStarted from "./GetStarted";
 import { AnimatedContent } from ".";
+import { useEffect, useState } from "react";
+import Button from "./Button";
 
 const Hero = () => {
+  const [content, setContent] = useState("");
+  useEffect(() => {
+    const arr = [
+      "F",
+      "U",
+      "L",
+      "L",
+      " ",
+      "S",
+      "T",
+      "A",
+      "C",
+      "K",
+      " ",
+      "D",
+      "E",
+      "V",
+      "E",
+      "L",
+      "O",
+      "P",
+      "E",
+      "R",
+    ];
+    let id = -1;
+    let end = false;
+    const interval = setInterval(() => {
+      id++;
+      setContent((current) => {
+        if (current?.length === 20) {
+          end = true;
+        } else if (current?.length === 0) {
+          end = false;
+          id = 0;
+        }
+        if (end) {
+          return current.slice(0, current?.length - 1);
+        } else {
+          return current + arr[id];
+        }
+      });
+    }, 150);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <AnimatedContent>
       <section
         id="home"
-        className={`flex md:flex-row flex-col ${styles.paddingY}`}
+        className={`flex md:flex-row flex-col ${styles.paddingY} h-[70vh]`}
       >
         <div
           className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
         >
-          <div className="flex flex-row justify-between items-center w-full">
-            <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-              The Next <br className="sm:block hidden" />{" "}
-              <span className="text-gradient">Generation</span>{" "}
-            </h1>
-            <div className="ss:flex hidden md:mr-4 mr-0">
-              <GetStarted />
+          <div className="flex flex-col justify-between items-center w-full  gap-16">
+            <div className="flex flex-col gap-2">
+              <div className="flex-1 font-poppins font-semibold ss:text-[68px]  flex flex-row gap-2 text-[28px] text-white  break-words ">
+                <span>
+                  {" "}
+                  Greetings,{" "}
+                  <span className="text-gradient">I'm Harsh Kumar</span>
+                </span>
+              </div>
+              <h2 className="text-5xl font-poppins ss:text-[50px] text-[28px] ">
+                <span className="text-gradient scale_animate"> {content}</span>
+                <span className="text-white">|</span>
+              </h2>
             </div>
+            <Button className="text-white border-teal-200" text="Hire Me" />
           </div>
-
-          <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-            Tool.
-          </h1>
-          <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-            Resolve issues, tailor conversations, collaborate with ease—and
-            build great customer relationships
-          </p>
-        </div>
-
-        <div
-          className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative h-[68vh]`}
-        >
-          <img
-            src={robot}
-            alt="billing"
-            className="w-[100%] h-[100%] relative z-[5]"
-            loading="eager"
-            preload
-          />
-
-          {/* gradient start */}
-          <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-          <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
-          <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-          {/* gradient end */}
-        </div>
-
-        <div className={`ss:hidden ${styles.flexCenter}`}>
-          <GetStarted />
         </div>
       </section>
     </AnimatedContent>
